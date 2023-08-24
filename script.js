74386cd4fbd302ee01abd5b1f373d2562c5bec0d
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2023-08-24 22:39:46 +0530

    Arrivals section

--- script.js
+++ script.js
@@ -80,3 +80,25 @@ var swiper = new Swiper(".featured-slider", {
         },
     },
 });
+
+var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 900,
+        disableOnInteraction: false,
+    },
+    
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
