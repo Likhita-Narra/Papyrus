d81f4f38c536e60a859ac92b2be096badbb723d9
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2023-08-24 20:45:28 +0530

    Home Section

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ window.onload = () =>{
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
+
+var swiper = new Swiper(".books-slider", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 900,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
