edd6709878f0ed231934226003b31f2da1c05a6c
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2023-08-24 12:10:49 +0530

    Responsive header and bottom navbar

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ document.querySelector("#search-btn").onclick = () =>{
 }
 
 window.onscroll = () =>{
+
+    searchForm.classList.remove('active');
+
     if(window.scrollY > 80) {
-        document.querySelector('.header .header-2')/classList.add('active');
+        document.querySelector('.header .header-2').classList.add('active');
     } else {
-        document.querySelector('.header .header-2')/classList.remove('active');
+        document.querySelector('.header .header-2').classList.remove('active');
     }
 }
 
 window.onload = () =>{
     if(window.scrollY > 80) {
-        document.querySelector('.header .header-2')/classList.add('active');
+        document.querySelector('.header .header-2').classList.add('active');
     } else {
-        document.querySelector('.header .header-2')/classList.remove('active');
+        document.querySelector('.header .header-2').classList.remove('active');
     }
 }
